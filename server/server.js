899f957e9ea904d2eec48bd2885bd3951779e869
Kyle Oba <koba@mudphone.com>
2013-07-12 02:03:58 -0700

    Got protocol buffers working with GTFS real time feed data.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,59 @@
-// server: populate collections with some initial documents
-// Vehicles.insert({vehicleId: ""});
-// var myVehicles = Vehicles.find({}).fetch();
+var path = Npm.require('path');
+var fs = Npm.require('fs');
+var base = path.resolve('.');
+var isBundle = fs.existsSync(base + '/bundle');
+var modulePath = base + (isBundle ? '/bundle/static' : '/public') + '/node_modules';
+ProtoBuf = Npm.require(modulePath + '/protobufjs'); // NOTE, this is going to be a global variable
+var builder = ProtoBuf.protoFromFile(base + "/public/data/gtfs-realtime.proto");
+if (_U.existy(builder)) {
+  console.log("builer ->");
+  console.log(builder);
+} else {
+  console.log("no builder!");
+}
+
+var http = Npm.require('http');
+
+Meteor.startup(function () {
+
+  var options = {
+    hostname: 'webapps.thebus.org',
+    port: 80,
+    path: '/transitdata/production/vehloc/',
+    method: 'GET'
+  };
+  var req = http.get(options, function (res) {
+    var data = [];
+
+    res.on('data', function (chunk) {
+      data.push(chunk);
+
+    }).on('end', function() {
+      //at this point data is an array of Buffers
+      //so we take each octet in each of the buffers
+      //and combine them into one big octet array to pass to a
+      //new buffer instance constructor
+      var buffer = new Buffer(data.reduce(function(prev, current) {
+          return prev.concat(Array.prototype.slice.call(current));
+        }, []));
+      var GTFSRealTime = builder.build("transit_realtime");
+      var decoder = GTFSRealTime.FeedMessage;
+      var buff = new Buffer(buffer, "base64");
+      console.log("Buffer.isBuffer: " + Buffer.isBuffer(buff));
+      var msg = decoder.decode(buff);
+      // console.log(msg);
+      console.log(msg.entity[0].vehicle);
+    });
+  });
+
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
+
+  // write data to request body
+  req.end();
+});
+
 
 // Honolulu:
 // Meteor.call('createUpdateVehicle', { vehicleId: "123", lastUpdate: Date.now(), latitude: 21.3069, longitude: -157.9583 })
